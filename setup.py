ac2afce056a1bc6d7627e42ddb998f1faa18ab90
Kenneth Reitz <me@kennethreitz.org>
2017-01-27 13:11:44 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+
+from setuptools import setup
+
+try:
+    # Python 3
+    from os import dirname
+except ImportError:
+    # Python 2
+    from os.path import dirname
+
+here = os.path.abspath(dirname(__file__))
+
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = [
+    'boto3',
+]
+
+setup(
+    name='bucketstore',
+    version='0.1.0',
+    description='A simple library for interacting with Amazon S3.',
+    long_description=long_description,
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/bucketstore',
+    py_modules=['bucketstore'],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+
+    ),
+)
