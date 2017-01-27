65ef27769fb5a8425a79354466c82e3b64d499fd
Kenneth Reitz <me@kennethreitz.org>
2017-01-27 16:03:44 -0500

    v0.1.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ required = [
 
 setup(
     name='bucketstore',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple library for interacting with Amazon S3.',
     long_description=long_description,
     author='Kenneth Reitz',
