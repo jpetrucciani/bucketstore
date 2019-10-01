14e7cbe53044c61886dd0f7af683dd1a88850693
Jacobi Petrucciani <j@mimir.cloud>
2019-10-01 14:11:38 -0400

    adding key download and upload, as well as size. adding 100% test coverage, and updating readme to show the new methods

--- bucketstore.py
+++ bucketstore.py
@@ -1,16 +1,18 @@
 """
 bucketstore module
 """
+import io
 import os
+import os.path
 import boto3
 import botocore
-from typing import List
+from typing import BinaryIO, Callable, List, Union
 
 
 AWS_DEFAULT_REGION = "us-east-1"
 
 
-class S3Key(object):
+class S3Key:
     """An Amazon S3 Key"""
 
     def __init__(self, bucket: "S3Bucket", name: str) -> None:
@@ -23,6 +25,10 @@ class S3Key(object):
         """str representation of an s3key"""
         return "<S3Key name={0!r} bucket={1!r}>".format(self.name, self.bucket.name)
 
+    def __len__(self) -> int:
+        """returns the size of the s3 object of this key in bytes"""
+        return self.size()
+
     @property
     def _boto_object(self):  # type: ignore
         """the underlying boto3 s3 key object"""
@@ -31,6 +37,36 @@ class S3Key(object):
     def get(self) -> str:
         """Gets the value of the key."""
         return self._boto_object.get()["Body"].read()
+
+    def download(self, file: Union[str, BinaryIO], callback: Callable = None) -> None:
+        """download the key to the given path or file object"""
+        if self.name not in self.bucket:
+            raise Exception("this key does not exist!")
+
+        _download = self.bucket._boto_s3.meta.client.download_fileobj
+        if isinstance(file, str):
+            with open(file, "wb") as data:
+                _download(self.bucket.name, self.name, data, Callback=callback)
+        elif isinstance(file, io.IOBase):
+            _download(self.bucket.name, self.name, file, Callback=callback)
+
+    def upload(self, file: Union[str, BinaryIO], callback: Callable = None) -> None:
+        """upload the file or file obj at the given path to this key"""
+        _upload = self.bucket._boto_s3.meta.client.upload_fileobj
+        if isinstance(file, str):
+            if not os.path.isfile(file):
+                raise Exception("file does not exist!")
+            with open(file, "rb") as data:
+                _upload(data, self.bucket.name, self.name, Callback=callback)
+        elif isinstance(file, io.IOBase):
+            _upload(file, self.bucket.name, self.name, Callback=callback)
+
+    def size(self) -> int:
+        """get the size of this object in s3"""
+        total = 0
+        for key in self.bucket._boto_bucket.objects.filter(Prefix=self.name):
+            total += key.size
+        return total
 
     def set(self, value: str, metadata: dict = None, content_type: str = "") -> dict:
         """Sets the key to the given value."""
@@ -41,13 +77,13 @@ class S3Key(object):
         )
 
     def rename(self, new_name: str) -> None:
-        """Renames the key to a given new name."""
-        # Write the new object.
-        self.bucket.set(new_name, self.get(), self.meta)
-
+        """renames the key to a given new name"""
+        # copy the item to avoid pulling and pushing
+        self.bucket._boto_s3.Object(self.bucket.name, new_name).copy_from(
+            CopySource="{}/{}".format(self.bucket.name, self.name)
+        )
         # Delete the current key.
         self.delete()
-
         # Set the new name.
         self.name = new_name
 
@@ -90,12 +126,11 @@ class S3Key(object):
                 self.bucket.name,
                 self.name,
             )
-        else:
-            raise ValueError(
-                "{0!r} does not have the public-read ACL set. "
-                "Use the make_public() method to allow for "
-                "public URL sharing.".format(self.name)
-            )
+        raise ValueError(
+            "{0!r} does not have the public-read ACL set. "
+            "Use the make_public() method to allow for "
+            "public URL sharing.".format(self.name)
+        )
 
     def temp_url(self, duration: int = 120) -> str:
         """returns a temporary URL for the given key."""
@@ -106,7 +141,7 @@ class S3Key(object):
         )
 
 
-class S3Bucket(object):
+class S3Bucket:
     """An Amazon S3 Bucket."""
 
     def __init__(self, name: str, create: bool = False, region: str = "") -> None:
@@ -141,13 +176,11 @@ class S3Bucket(object):
         try:
             self._boto_s3.Object(self.name, item).load()
             return True
-        except botocore.exceptions.ClientError as e:
-            if e.response["Error"]["Code"] == "404":
+        except botocore.exceptions.ClientError as exception:
+            if exception.response["Error"]["Code"] == "404":
                 # The object does not exist.
                 return False
-            else:
-                # Something else has gone wrong.
-                raise
+            raise  # pragma: no cover
 
     def list(self) -> List:
         """returns a list of keys in the bucket."""
--- tests/test_bucketstore.py
+++ tests/test_bucketstore.py
@@ -6,10 +6,11 @@ import json
 import os
 import pytest
 import sys
+import tempfile
 from moto import mock_s3
 
 
-def dbg(text):
+def dbg(text: str) -> None:
     """debug printer for tests"""
     if isinstance(text, dict):
         text = json.dumps(text, sort_keys=True, indent=2)
@@ -21,7 +22,7 @@ def dbg(text):
     print("")
 
 
-def test_login():
+def test_login() -> None:
     """
     Ensure that login sets the correct environment variables.
 
@@ -33,7 +34,7 @@ def test_login():
 
 
 @mock_s3
-def test_buckets_can_be_created():
+def test_buckets_can_be_created() -> None:
     bucket = bucketstore.get("test-bucket", create=True)
 
     assert bucket.name == "test-bucket"
@@ -43,16 +44,16 @@ def test_buckets_can_be_created():
 
 
 @mock_s3
-def test_buckets_are_not_created_automatically():
+def test_buckets_are_not_created_automatically() -> None:
     with pytest.raises(ValueError):
         bucketstore.get("non-existent-bucket")
 
 
-def test_buckets_can_be_listed(bucket):
+def test_buckets_can_be_listed(bucket: bucketstore.S3Bucket) -> None:
     assert bucket.name in bucketstore.list()
 
 
-def test_buckets_can_be_deleted(bucket):
+def test_buckets_can_be_deleted(bucket: bucketstore.S3Bucket) -> None:
     bucket["foo"] = "bar"
     bucket.delete()
 
@@ -62,14 +63,14 @@ def test_buckets_can_be_deleted(bucket):
         bucket.all()
 
 
-def test_buckets_can_be_made_public(bucket):
+def test_buckets_can_be_made_public(bucket: bucketstore.S3Bucket) -> None:
     assert not bucket.is_public
 
     bucket.make_public()
     assert bucket.is_public
 
 
-def test_buckets_can_set_keys(bucket):
+def test_buckets_can_set_keys(bucket: bucketstore.S3Bucket) -> None:
     # Buckets can set keys with a function
     bucket.set("foo", "bar")
     assert bucket.get("foo") == b"bar"
@@ -79,19 +80,25 @@ def test_buckets_can_set_keys(bucket):
     assert bucket["foo2"] == b"bar2"
 
 
-def test_keys_can_be_renamed(bucket):
+def test_keys_can_be_renamed(bucket: bucketstore.S3Bucket) -> None:
+    """test renaming keys"""
     bucket.set("original_name", "value")
     bucket.key("original_name").rename("new_name")
     assert bucket["new_name"] == b"value"
 
+    # check that the original was removed
+    assert "original_name" not in bucket
 
-def test_keys_can_be_deleted(bucket):
+
+def test_keys_can_be_deleted(bucket: bucketstore.S3Bucket) -> None:
+    """test deleting a key"""
     bucket["foo"] = "bar"
     bucket.delete("foo")
     assert not bucket.all()
 
 
-def test_keys_can_be_made_public(key):
+def test_keys_can_be_made_public(key: bucketstore.S3Key) -> None:
+    """make sure keys are private by default, but able to be public"""
     # Keys are private by default.
     assert not key.is_public
 
@@ -100,7 +107,8 @@ def test_keys_can_be_made_public(key):
     assert key.is_public
 
 
-def test_keys_can_be_linked_to(key):
+def test_keys_can_be_linked_to(key: bucketstore.S3Key) -> None:
+    """test that we can link to keys using temp_url and url on public keys"""
     # A public link is going to fail because it's private.
     with pytest.raises(ValueError):
         assert key.url
@@ -117,9 +125,12 @@ def test_keys_can_be_linked_to(key):
     assert "http" in key.url
     assert key.name in key.url
 
+    # empty response for trying to make a public key public
+    assert key.make_public() == {}
 
-def test_keys_have_metadata(key):
-    # Metadata is empty by default
+
+def test_keys_have_metadata(key: bucketstore.S3Key) -> None:
+    """Metadata is empty by default"""
     assert key.meta == {}
 
     metadata = {"foo": "bar"}
@@ -127,46 +138,99 @@ def test_keys_have_metadata(key):
     assert key.meta == metadata
 
 
-def test_keys_have_a_cool_repr(key):
-    # The textual representation of the class is nifty, so test it.
+def test_keys_have_a_cool_repr(key: bucketstore.S3Key) -> None:
+    """The textual representation of the class is nifty, so test it."""
     rep = repr(key)
     assert "S3Key" in rep
     assert key.name in rep
     assert key.bucket.name in rep
 
 
-def test_private_methods(key):
-    # This method contains boto internals, so as long as it returns a truthy
-    # value, it is good.
+def test_private_methods(key: bucketstore.S3Key) -> None:
+    """This method contains boto internals, so as long as it returns a truthy value, it is good."""
     assert key._boto_object
 
 
-def test_bucket_keys_can_be_iterated_upon(bucket):
-    # Create 10 keys
-    for idx in range(10):
-        bucket[str(idx)] = str(idx)
+def test_bucket_keys_can_be_iterated_upon(bucket: bucketstore.S3Bucket) -> None:
+    """Create 10 keys"""
+    for index in range(10):
+        bucket[str(index)] = str(index)
 
     keys = bucket.all()
     assert len(keys) == 10
 
-    for idx, key in enumerate(keys):
-        assert key.name == str(idx)
+    for index, key in enumerate(keys):
+        assert key.name == str(index)
 
 
-def test_bucket_in_operator(bucket):
-    """test that we can use the in operator on a bucket"""
-    for idx in range(10):
-        bucket[str(idx)] = str(idx)
+def test_bucket_in_operator(bucket: bucketstore.S3Bucket) -> None:
+    """test that we can use the `in` operator on a bucket"""
+    for index in range(10):
+        bucket[str(index)] = str(index)
 
     assert "0" in bucket
     assert "10" not in bucket
 
 
-def test_bucket_del_operator(bucket):
-    """test that we can use the del operator on a bucket + key"""
-    for idx in range(10):
-        bucket[str(idx)] = str(idx)
+def test_bucket_del_operator(bucket: bucketstore.S3Bucket) -> None:
+    """test that we can use the `del` operator on a bucket + key"""
+    for index in range(10):
+        bucket[str(index)] = str(index)
 
     assert "0" in bucket
     del bucket["0"]
     assert "0" not in bucket
+
+
+def test_key_upload(bucket: bucketstore.S3Bucket) -> None:
+    """test that we can use the key upload function"""
+    path = "test_upload_file_path"
+    # test not a file
+    with pytest.raises(Exception):
+        bucket.key(path).upload("/this/is/not/a/file")
+
+    # test uploading from a file
+    bucket.key(path).upload("LICENSE")
+    assert path in bucket
+    assert "MIT License" in str(bucket[path])
+
+    # test uploading a file object
+    path = "test_upload_file_obj"
+    with open("LICENSE", "rb") as temp:
+        bucket.key(path).upload(temp)
+    assert path in bucket
+    assert "MIT License" in str(bucket[path])
+
+
+def test_key_download(bucket: bucketstore.S3Bucket, key: bucketstore.S3Key) -> None:
+    """test that we can use the key download function"""
+    # test key doesn't exist
+    with pytest.raises(Exception):
+        bucket.key("this/is/not/a/key").download("/this/is/not/a/file")
+
+    # test downloading to a file
+    _, path = tempfile.mkstemp()
+    key.download(path)
+    with open(path, "r") as file_0:
+        data = file_0.read()
+        assert isinstance(data, str)
+        assert "a testing value" in data
+    os.remove(path)
+
+    # test downloading to a file object
+    _, path = tempfile.mkstemp()
+    with open(path, "wb") as file_1:
+        key.download(file_1)
+    with open(path, "r") as file_2:
+        data = file_2.read()
+        assert isinstance(data, str)
+        assert "a testing value" in data
+    os.remove(path)
+
+
+def test_key_size(key: bucketstore.S3Key) -> None:
+    """test getting the size of a key"""
+    size = key.size()
+    assert size
+    assert size == 15
+    assert size == len(key)
